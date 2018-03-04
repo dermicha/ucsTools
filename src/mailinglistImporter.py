fa99f162f2266bbc2a4500e70f6c7e35543ea73f
derMicha <dermicha@dermicha.de>
2018-03-04 17:17:15 +0100

    fixed email clenup

--- src/mailinglistImporter.py
+++ src/mailinglistImporter.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Extend UCS mailing lists')
 parser.add_argument('-ln', '--listname', help="name of listobject (cn)", metavar="CN", required=True)
 parser.add_argument('-dc', '--domaincomponent', help="domain component (dc)", metavar="DC", default="esbmz")
 parser.add_argument('-f', '--filename', help="filename of csv file", metavar="FILE", required=True)
-parser.add_argument('-o', '--offset', help="number of column which contains a mailadress to be imported",
+parser.add_argument('-o', '--offset', help="number of column which contains a mailadress to be imported (starting with 0)",
                     metavar="OFFSET", type=int, required=True)
 parser.add_argument('-s', '--separator', help="csv file separator", metavar="SEPARATOR", default=";")
 parser.add_argument('-dr', '--dryrun', help="dryrun just prints statements", action='store_true')
@@ -34,7 +34,7 @@ inputFile = open(filename, 'r', newline='\n', encoding='UTF-8')
 for line in inputFile:
     cols = line.split(sep)
     if offset <= len(cols):
-        email = cols[offset].lower()
+        email = cols[offset].lower().replace(" ", "").replace(",", "").replace(";", "")
         if (email != "" and "@" in email and "." in email):
             c = "{}{}".format(cmd, email)
             if (drun):
